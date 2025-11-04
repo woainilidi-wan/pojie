a434e8eb34c133d99d33598701a43826f5ad8e3a
woainilidi-wan <woainilidi@gmail.com>
2025-11-04 13:20:57 +0800

    更新 hb.js

--- hb.js
+++ hb.js
@@ -3,7 +3,7 @@
  *
 *******************************
 [rewrite_local]
-^https?:\/\/h5\.hunbei\.com\/m\/member\/getUserInfo?token=ecdaf40fddf1ce517bce0d0795f45774&uid=1137065&version=4\.27\.1 url script-response-body https://raw.githubusercontent.com/woainilidi-wan/pojie/refs/heads/main/hb.js
+^https?:\/\/h5\.hunbei\.com\/m\/member\/getUserInfo url script-response-body https://raw.githubusercontent.com/woainilidi-wan/pojie/refs/heads/main/hb.js
 [mitm] 
 hostname = h5.hunbei.com
 *
