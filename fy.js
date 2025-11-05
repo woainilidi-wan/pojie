a180734a15092a2582182816e6eea8200c3e88a6
woainilidi-wan <woainilidi@gmail.com>
2025-11-06 02:05:47 +0800

    更新 fy.js

--- fy.js
+++ fy.js
@@ -19,7 +19,7 @@ obj = {
   "data" : {
     "id" : 58589,
     "uuid" : "MHBJFSXESJCEFJGINMVDTTYUU",
-    "vip" : 3,
+    "vip" : 1,
     "viptime" : "1999999999",
     "ispingjia" : 0,
     "area" : "",
