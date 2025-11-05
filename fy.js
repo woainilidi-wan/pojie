5f1958d1498cccbbced5596916714f71d3c993c2
woainilidi-wan <woainilidi@gmail.com>
2025-11-06 01:50:10 +0800

    创建 fy.js

---
+++ fy.js
@@ -0,0 +1,37 @@
+/*
+ *
+ *
+*******************************
+
+[rewrite_local]
+^https?:\/\/www\.ldjsf\.site\/api\/LDisvip url script-response-body https://raw.githubusercontent.com/woainilidi-wan/pojie/refs/heads/main/fy.js
+[mitm] 
+hostname = www.ldjsf.site
+
+*
+*
+*/
+
+var body = $request.body; 
+let obj = JSON.parse($request.body);
+obj = {
+  "msg" : "查询成功",
+  "data" : {
+    "id" : 58589,
+    "uuid" : "MHBJFSXESJCEFJGINMVDTTYUU",
+    "vip" : 2,
+    "viptime" : "9999999999",
+    "ispingjia" : 0,
+    "area" : "",
+    "isnew" : 0,
+    "expiration" : "(2099-09-09)",
+    "token" : "9ae1a065dfa37102ebc42969061b2d7d",
+    "del" : 1,
+    "username" : "68856855",
+    "email" : "2720566638@qq.com",
+    "score" : 0
+  },
+  "code" : "200"
+}
+
+$done({body: JSON.stringify(obj)});
