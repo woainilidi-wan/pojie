e74e8eaa3bc2f0172f736e6dbefabde8a125442e
woainilidi-wan <woainilidi@gmail.com>
2025-11-12 20:03:47 +0800

    更新 ktx.js

--- ktx.js
+++ ktx.js
@@ -16,15 +16,49 @@ hostname = ktx.cn
   "rewrite": [
     {
       "matches": "^https?://ktx\\.cn/v3/api/my/home/get_home_center\\?ktxToken=[a-fA-F0-9]+",
-      "name": "我是大帅哥",
+      "name": "KTX VIP 信息修改",
       "script": {
         "type": "response",
         "body": `
           let obj = JSON.parse($response.body);
+          
+          // 修改 VIP 状态
           obj.isVip = 1;
+          
+          // 修改用户积分和经验值
           obj.point = 9999;
           obj.exp = 8888;
+          
+          // 修改用户等级
           obj.level = 5;
+          
+          // 修改优惠券数量
+          obj.couponCountUsed = 0;
+          obj.couponCountAboutToExpire = 5;
+          obj.askEditorCardCount = 3;
+          obj.feedbackCount = 0;
+          obj.waitPayOrder = 0;
+          
+          // 添加或修改 VIP 订阅信息
+          const now = Date.now();
+          obj.subscriptionVip = {
+            "userId": obj.user.id,
+            "beginTime": now,
+            "id": 256145,
+            "endTime": now + 365 * 24 * 60 * 60 * 1000, // 一年后
+            "createTimeFormat": new Date().toLocaleString("zh-CN").replace(/\//g, '-'),
+            "createTime": now,
+            "isActive": 1
+          };
+          
+          // 修改提示信息
+          obj.info = "尊享VIP特权，畅享无限福利";
+          
+          // 确保状态成功
+          obj.status = 1;
+          obj.msg = "success";
+          
+          // 返回修改后的内容
           $done({body: JSON.stringify(obj)});
         `
       }
