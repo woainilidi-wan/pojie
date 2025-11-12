70c9aaced10d181278ee2f868bd07e9bc4f7b024
woainilidi-wan <woainilidi@gmail.com>
2025-11-12 19:46:57 +0800

    更新 ktx.js

--- ktx.js
+++ ktx.js
@@ -12,10 +12,49 @@ hostname = ktx.cn
 *
 */
 
-var body = $request.body; 
-var obj = JSON.parse($request.body);
-obj.subscriptionVip.endTime = 1999999999999,
-obj. subscriptionVip.createTimeFormat = "2099-11-12 16:15:13",
-obj.isVip = 1,
-obj.level = 1,
-$done({body: JSON.stringify(obj)});
+{
+  "rewrite": [
+    {
+      "matches": "^https?://.*\\.(your-api-domain|example)\\.com/api/user/info", // 请替换为实际的 API 域名
+      "name": "VIP 信息修改",
+      "script": {
+        "type": "response",
+        "body": `
+          let obj = JSON.parse($response.body);
+          
+          // 修改 VIP 状态
+          obj.isVip = 1;
+          
+          // 修改用户积分和经验值
+          obj.point = 9999;
+          obj.exp = 8888;
+          
+          // 修改用户等级
+          obj.level = 5;
+          
+          // 修改优惠券数量
+          obj.couponCountUsed = 0;
+          obj.couponCountAboutToExpire = 5;
+          obj.askEditorCardCount = 3;
+          
+          // 添加或修改 VIP 订阅信息
+          obj.subscriptionVip = {
+            "userId": obj.user.id,
+            "beginTime": Date.now(),
+            "id": 256145,
+            "endTime": Date.now() + 365 * 24 * 60 * 60 * 1000, // 一年后
+            "createTimeFormat": new Date().toLocaleString("zh-CN"),
+            "createTime": Date.now(),
+            "isActive": 1
+          };
+          
+          // 修改提示信息
+          obj.info = "尊享VIP特权，畅享无限福利";
+          
+          // 返回修改后的内容
+          $done({body: JSON.stringify(obj)});
+        `
+      }
+    }
+  ]
+}
