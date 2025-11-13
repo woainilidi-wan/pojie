dae4f60eed249b327e69cf3d4cdb58364e072d16
woainilidi-wan <woainilidi@gmail.com>
2025-11-14 00:16:16 +0800

    更新 xldc.js

--- xldc.js
+++ xldc.js
@@ -15,25 +15,11 @@ hostname = www.wordvector.com
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// 解锁所有权限
-obj.data.can = {
-  "content": true,
-  "trade": true,
-  "edit": true
-};
-
-// 添加 VIP 特权
-obj.data.user_info.vip_info = {
-  "is_vip": true,
-  "vip_end_time": "1999999999,
-  "have_days": 36500,
-};
-
-// 升级用户角色
-obj.data.user_info.role = "VIP用户";
-obj.data.user_info.role_name = "VIP用户";
-
-// 添加欢迎提示
-obj.data.tip_list = ["欢迎使用VIP特权！", "尊享会员权益已开启"];
+// 直接替换整个 can 对象
+obj.data.can = {"content": true, "trade": true, "edit": true};
+// 替换 VIP 信息
+obj.data.user_info.vip_info = {"is_vip": true, "vip_end_time": "2026-12-31", "have_days": 365};
+obj.data.user_info.role = obj.data.user_info.role_name = "VIP用户";
+obj.data.tip_list = ["VIP特权已激活"];
 
 $done({body: JSON.stringify(obj)});
